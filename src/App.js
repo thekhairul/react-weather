c2b7357374aeef4881505b16ce85380ed7a5ceb4
khairul_anik <anik@softograph.com>
2019-04-16 17:54:32 +0600

    implemented css modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./components/weather/Weather";
 
 class App extends Component {
   render() {
@@ -8,17 +9,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Weather
+            day="Sunday"
+            thumbnail="https://google.com"
+            minTemp="34"
+            maxTemp="43"
+          />
         </header>
       </div>
     );
--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./Weather.module.scss";
 
 const Weather = ({ day, thumbnail, minTemp, maxTemp }) => {
   return (
-    <div className="weather" id="weather">
+    <div className={styles.weather} id="weather">
       <span className="weather-min">{minTemp}</span>
       <h2 className="weather-day">{day}</h2>
       <img src={thumbnail} alt="" />
