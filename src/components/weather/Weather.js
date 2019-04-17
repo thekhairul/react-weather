dcfed03d4d90f7fbd797a67b60f35584f28f747c
thekhairul <thekhairul.office@gmail.com>
2019-04-17 08:50:10 +0600

    added axios

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -3,7 +3,7 @@ import styles from "./Weather.module.scss";
 
 const Weather = ({ day, thumbnail, minTemp, maxTemp }) => {
   return (
-    <div className={styles.weather} id="weather">
+    <div className="weather" id="weather">
       <span className="weather-min">{minTemp}</span>
       <h2 className="weather-day">{day}</h2>
       <img src={thumbnail} alt="" />
