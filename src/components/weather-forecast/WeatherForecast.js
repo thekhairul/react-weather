d09aa3745f580d9fb483441a6edf019acc6911f4
khairul_anik <anik@softograph.com>
2019-07-24 22:59:32 +0600

    change app bg

--- src/components/weather-forecast/WeatherForecast.js
+++ src/components/weather-forecast/WeatherForecast.js
@@ -1,6 +1,11 @@
 // TODO: chart js fontawesome https://stackoverflow.com/questions/36979474/how-to-change-the-labels-to-the-image-icon-in-bar-chart-js
 import React from "react";
 import { Line } from "react-chartjs-2";
+import { defaults } from "react-chartjs-2";
+import styles from "./WeatherForecast.module.scss";
+
+// chart js default font color
+defaults.global.defaultFontColor = "black";
 
 const WeatherForecast = ({ labels, data }) => {
   const chartOptions = {
@@ -12,10 +17,28 @@ const WeatherForecast = ({ labels, data }) => {
           if (label) {
             label += ": ";
           }
-          label += Math.round(tooltipItem.yLabel * 100) / 100;
-          return label;
+          label += Math.round(tooltipItem.yLabel);
+          return label + "*C";
         }
       }
+    },
+    scales: {
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "Time"
+          }
+        }
+      ],
+      yAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "Temperature"
+          }
+        }
+      ]
     }
   };
   const chartData = {
@@ -26,26 +49,26 @@ const WeatherForecast = ({ labels, data }) => {
         fill: false,
         lineTension: 0.1,
         backgroundColor: "rgba(0,0,0,1)",
-        borderColor: "rgba(255,255,255,1)",
+        borderColor: "rgba(0,0,0,1)",
         borderCapStyle: "butt",
         borderDash: [],
         borderDashOffset: 0.0,
         borderJoinStyle: "miter",
-        pointBorderColor: "rgba(75,192,192,1)",
+        pointBorderColor: "rgba(0,0,0,1)",
         pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
+        pointBorderWidth: 4,
         pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75,192,192,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
+        pointHoverBackgroundColor: "rgba(255,255,255,1)",
+        pointHoverBorderColor: "rgba(255,255,255,1)",
+        pointHoverBorderWidth: 5,
+        pointRadius: 5,
+        pointHitRadius: 8,
         data: data
       }
     ]
   };
   return (
-    <div className="weather-forecast">
+    <div className={styles["weather-forecast"]}>
       <Line data={chartData} options={chartOptions} />
     </div>
   );
