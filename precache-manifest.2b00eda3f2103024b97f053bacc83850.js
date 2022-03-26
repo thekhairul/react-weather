469e28d87f5ed0873afc25a11ce62579bb758d00
Khairul Anik <khairulanik@Khairuls-MacBook-Air.local>
2022-03-26 17:20:12 +0600

    Updates

---
+++ precache-manifest.2b00eda3f2103024b97f053bacc83850.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "a81d7cda1b0cd0512041",
+    "url": "/static/css/main.f0e0104b.chunk.css"
+  },
+  {
+    "revision": "a81d7cda1b0cd0512041",
+    "url": "/static/js/main.1a90ee7c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9351464c895e0473dcf9",
+    "url": "/static/js/2.c183cb97.chunk.js"
+  },
+  {
+    "revision": "28f3b97164c3f20bb2440fd8f8299e42",
+    "url": "/static/media/icon-wind.28f3b971.svg"
+  },
+  {
+    "revision": "0f861d8593896bae4d68c5fad8e278cf",
+    "url": "/static/media/icon-humidity.0f861d85.svg"
+  },
+  {
+    "revision": "5a51538d9ffc8ddd9b1d6dbabd28dc27",
+    "url": "/static/media/icon-pressure.5a51538d.svg"
+  },
+  {
+    "revision": "b1b044b3b393c89ee2e0889365448d93",
+    "url": "/static/media/icon-visibility.b1b044b3.svg"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "1ef19f1b7c635b684456c9ab04f285e4",
+    "url": "/static/media/app-bg.1ef19f1b.jpg"
+  },
+  {
+    "revision": "ac135ba9a861c896471ac8c396ff72b7",
+    "url": "/index.html"
+  }
+];
